a5eee73bb5ffd41bc516dda22d798c942524a5b2
Daniel Kipp <daniel.kipp@gmail.com>
2024-11-15 15:07:33 +0100

    refactoring

--- lua/nvim-tablecalc/parsing.lua
+++ lua/nvim-tablecalc/parsing.lua
@@ -4,30 +4,31 @@ local utils = require('nvim-tablecalc.utils')
 
 -- Parses a structured table with headers and stores it in a nested format
 function M.parse_structured_table(content)
-  M.rows = {} -- Global array to store all parsed tables
+  M.rows = {}                   -- Global array to store all parsed tables
   local current_table_name = "" -- Variable to keep track of the current table name
-  local headers = {} -- Array to store the column headers of the current table
+  local headers = {}            -- Array to store the column headers of the current table
 
   -- Split content into lines and process each line
   for line in content:gmatch("[^\r\n]+") do
     -- Detect table names, marked by a line starting with `#`
     if line:match("^#") then
-      current_table_name = line:match("#%s*.-%s(%w+)%s*$") -- Extract the table name
+      -- Extract the table name, the last word in a row beginning with #
+      current_table_name = line:match("#%s*.-%s(%w+)%s*$")
       M.rows[current_table_name] = {} -- Initialize a table for the extracted name
-      headers = {} -- Reset headers for the new table
+      headers = {}                    -- Reset headers for the new table
     elseif line:match(config.delimiter) then
       -- If headers are not set, parse the current line as the header row
       if #headers == 0 then
         for header in line:gmatch(config.delimiter .. "%s*([^" .. config.delimiter .. "]+)%s*") do
+          -- Extract last word to use as alias
           header = header:match("%s*(%w+)%s*$")
-          print(header)
-          table.insert(headers, utils.stripe(header)) -- Clean and add header
+          table.insert(headers, utils.stripe(header))           -- Clean and add header
           M.rows[current_table_name][utils.stripe(header)] = {} -- Create sub-tables for headers
         end
       else
         -- Parse table rows and map values to their corresponding headers
         local row_index = tonumber(line:match("|%s*(%d+)")) -- Extract the row index
-        local col_index = 1 -- Track the column index for mapping
+        local col_index = 1                                 -- Track the column index for mapping
         for value in line:gmatch("|%s*([^|]+)%s*") do
           local header = headers[col_index]
           if row_index then
