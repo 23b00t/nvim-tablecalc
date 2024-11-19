c03e87566f1cb1367f3496e7770c0072cbfe5ae6
Daniel Kipp <daniel.kipp@gmail.com>
2024-11-19 12:51:21 +0100

    added option to configure filetypes to lazylaod by user

--- plugin/nvim-tablecalc.lua
+++ plugin/nvim-tablecalc.lua
@@ -5,15 +5,22 @@ if vim.g.loaded_nvim_tablecalc then
 end
 vim.g.loaded_nvim_tablecalc = true
 
+-- Function to initialize the TableCalc plugin lazily
 local function lazy_load_tablecalc()
   local tablecalc = require("nvim-tablecalc").get_instance()
   tablecalc:setup()
   return tablecalc
 end
 
+-- INFO: Customizable filetypes in user config with:
+-- vim.g.tablecalc_filetypes = { "org", "text", "markdown" }
+-- Set a default value for tablecalc_filetypes if not already defined
+vim.g.tablecalc_filetypes = vim.g.tablecalc_filetypes or { "org", "markdown" }
+
+-- Autocommand to load the plugin lazily for the specified filetypes
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "org", "md" },
+  pattern = vim.g.tablecalc_filetypes,  -- Use the user-defined or default filetypes
   callback = function()
-    lazy_load_tablecalc()
+    lazy_load_tablecalc()  -- Load the plugin when the filetype matches
   end
 })
