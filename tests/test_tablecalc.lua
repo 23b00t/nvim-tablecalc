e089fa9efb1945ed5bd4590196e31c391b5e5cc5
Daniel Kipp <daniel.kipp@gmail.com>
2024-11-25 14:51:41 +0100

    load TableCalc in test_suite

--- tests/test_tablecalc.lua
+++ tests/test_tablecalc.lua
@@ -1,9 +1,6 @@
 local luaunit = require('luaunit')
 
--- Extending package.path to correctly set the directory for modules
-package.path = package.path ..
-    ";/home/user/code/lua/nvim-tablecalc/lua/?.lua;/home/user/code/lua/nvim-tablecalc/lua/?/init.lua"
-local TableCalc = require('nvim-tablecalc.init')
+local TableCalc = _G.TableCalc
 
 -- Defining the test suite
 TestTableCalc = {}
